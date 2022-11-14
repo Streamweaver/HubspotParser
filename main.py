f17bc7e3c82f386c273a87d8319d75545a85dd2a
Scott Turnbull <streamweaver@gmail.com>
2022-11-14 09:37:38 -0500

    Adding some contact parsing to test.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import islice
 
 def parse_company_name(data):
     result = ""
@@ -41,8 +42,8 @@ def parse_company_record(data):
     Parses a single company record from raw data
     """
     record = {
+        'Company Domain': f'{data["Program"]}{data["Team Number"]}.org',
         'Company Name': parse_company_name(data),
-        'Company Domain Name': parse_company_name(data),
         'City': data["Team City"],
         'State': data["Team State Province"],
         'Postal Code': data["Team Postal Code"],
@@ -57,12 +58,38 @@ def parse_company_record(data):
     return record
 
 
+
+def parse_company_contact_records(data, name_key, email_key, phone_key):
+    """
+    Parses contact records for the LC1, LC2, and Team Admin in the record.
+    """
+    record = {
+        'First Name': parse_first_name(data, name_key),
+        'Last Name': parse_last_name(data, name_key),
+        'Email': data[email_key],
+        'Phone': data[phone_key],
+        'Company Name': parse_company_name(data),
+        'Type': 'Team Contact'
+    }
+    return record
+
+
 if __name__ == "__main__":
     companies = []
+    contacts = []
+    keys_list = [
+        ['LC1 Name', 'LC1 Email', 'LC1 Phone'],
+        ['LC2 Name', 'LC2 Email', 'LC2 Phone'],
+        ['Team Admin Name', 'Team Admin Email', 'Team Admin Phone']
+    ]
     with open('data/FullTeamData.csv', newline='',encoding='utf8') as data_csvfile:
         reader = csv.DictReader(data_csvfile)
-        for row in reader:
+        for row in islice(reader,10):
             companies.append(parse_company_record(row))
+            for key_list in keys_list:
+                if row[key_list[0]]:
+                    contacts.append(parse_company_contact_records(row, key_list[0], key_list[1], key_list[2]))
+
     with open('data/company_import_file.csv', 'w', newline='', encoding='utf8') as company_csvfile:
         fieldnames = companies[0].keys()
         writer = csv.DictWriter(company_csvfile, fieldnames=fieldnames)
@@ -70,3 +97,13 @@ if __name__ == "__main__":
         writer.writeheader()
         for company in companies:
             writer.writerow(company)
+
+    with open('data/contact_import_file.csv', 'w', newline='', encoding='utf8') as contact_csvfile:
+        fieldnames = contacts[0].keys()
+        writer = csv.DictWriter(contact_csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for contact in contacts:
+            writer.writerow(contact)
+
+
